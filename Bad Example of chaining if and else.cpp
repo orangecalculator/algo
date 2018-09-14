30aaabf2eeeded01dce69eb435aa29fcdf7aa2dd
orangecalculator <43122348+orangecalculator@users.noreply.github.com>
2018-09-15 00:48:06 +0900

    Update Bad Example of chaining if and else.cpp
    Description about the wrong code

--- Bad Example of chaining if and else.cpp
+++ Bad Example of chaining if and else.cpp
@@ -8,7 +8,59 @@ int nodeheap(int *sor, int i, int j, int p){
             if(sor[left]>=sor[right])
                 if(sor[left]>sor[p]) u=left;
             else if(sor[right]>sor[p]) u=right;
-            
+        else if(j<left);
+        else if(sor[left]>sor[p]) u=left;;
+
+        if(p!=u)swap(sor+p,sor+u);
+        return u;
+        }
+/*
+The original intention of chaining was grouping if and else level shown by indent
+However, C or C++ does not interpret in that way
+Rather, as follows
+*/
+
+int nodeheap(int *sor, int i, int j, int p){
+        int u=p,left=2*p-i+1;
+        int right=left+1;
+
+        if(j>=right)
+            if(sor[left]>=sor[right])
+                if(sor[left]>sor[p]) u=left;
+        	else if(sor[right]>sor[p]) u=right;
+        	else if(j<left);
+        	else if(sor[left]>sor[p]) u=left;
+
+        if(p!=u)swap(sor+p,sor+u);
+        return u;
+        }
+/*
+In conclusion, in the wrong script, the intended job wouldnt work if index is small or right value is bigger
+Therefore, the wrong script is the same as follows
+*/
+int nodeheap(int *sor, int i, int j, int p){
+        int u=p,left=2*p-i+1;
+        int right=left+1;
+
+        if(j>=right){
+            if(sor[left]>=sor[right])
+                if(sor[left]>sor[p]) u=left;
+
+        if(p!=u)swap(sor+p,sor+u);
+        return u;
+        }
+
+//The intended code is as follows
+
+int nodeheap(int *sor, int i, int j, int p){
+        int u=p,left=2*p-i+1;
+        int right=left+1;
+
+        if(j>=right){
+            if(sor[left]>=sor[right])
+                {if(sor[left]>sor[p]) u=left;}
+            else if(sor[right]>sor[p]) u=right;
+            }
             
         else if(j<left);
         else if(sor[left]>sor[p]) u=left;;
@@ -16,4 +68,3 @@ int nodeheap(int *sor, int i, int j, int p){
         if(p!=u)swap(sor+p,sor+u);
         return u;
         }
-
