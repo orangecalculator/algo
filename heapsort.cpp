2b5512b31406c11d187f917b1472de2d0f54f37c
orangecalculator <smartman1996@gmail.com>
2018-09-15 01:27:24 +0900

    optimize heapsort

--- heapsort.cpp
+++ heapsort.cpp
@@ -8,9 +8,12 @@ int swap(int *swap1,int *swap2){
         return 1;
 }
 
-int nodeheap(int *sor, int i, int j, int p){
-        int u=p,left=2*p-i+1;
-        int right=left+1;
+int downheap(int *sor, int i, int j, int p){
+        int u=p+1,left,right;
+        while(p!=u){
+            u=p;
+            left=2*p-i+1;
+            right=left+1;
 
         if(j>=right){
             if(sor[left]>=sor[right])
@@ -22,30 +25,13 @@ int nodeheap(int *sor, int i, int j, int p){
         else if(sor[left]>sor[p]) u=left;;
 
         if(p!=u)swap(sor+p,sor+u);
-        return u;
-        }
-
-int downheap(int *sor, int i, int j, int p){
-        int next=p;
-        p++;
-        while(p!=next){
-            p=next;
-            next=nodeheap(sor,i,j,p);
         }
         return 1;
-}
-
-int iniheapify(int *sor,int i,int j){
-
-        int s;
-        for(s=(j+i-1)/2;s>=i;s--)downheap(sor,i,j,s);
-        return 0;
 }
 
 int heapsort(int *sor,int i,int j){
         int s;
         for(s=(j+i-1)/2;s>=i;s--)downheap(sor,i,j,s);
-int k;
         for(s=j;s>i;s--){
             swap(sor+s,sor+i);
             downheap(sor,i,s-1,i);
