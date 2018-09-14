a279d0ba756f29fe81ca4ec700142a1ae24173d7
orangecalculator <43122348+orangecalculator@users.noreply.github.com>
2018-09-15 00:34:12 +0900

    Update heapsort.cpp
    heapsort debugging complete

--- heapsort.cpp
+++ heapsort.cpp
@@ -12,19 +12,22 @@ int nodeheap(int *sor, int i, int j, int p){
         int u=p,left=2*p-i+1;
         int right=left+1;
 
-        if(j>=right)
+        if(j>=right){
             if(sor[left]>=sor[right])
-                if(sor[left]>sor[p]) u=left;
+                {if(sor[left]>sor[p]) u=left;}
             else if(sor[right]>sor[p]) u=right;
-        else if(j<left) ;
-        else if(sor[left]>sor[p]) u=left;
+            }
+            
+        else if(j<left);
+        else if(sor[left]>sor[p]) u=left;;
 
         if(p!=u)swap(sor+p,sor+u);
         return u;
         }
 
 int downheap(int *sor, int i, int j, int p){
-        int next=p;p++;
+        int next=p;
+        p++;
         while(p!=next){
             p=next;
             next=nodeheap(sor,i,j,p);
@@ -42,14 +45,13 @@ int iniheapify(int *sor,int i,int j){
 int heapsort(int *sor,int i,int j){
         int s;
         for(s=(j+i-1)/2;s>=i;s--)downheap(sor,i,j,s);
-
-        for(s=j;s>i+1;s--){
-            printf("%d %d %d %d %d\n",sor[0],sor[1],sor[2],sor[3],sor[4]);
+int k;
+        for(s=j;s>i;s--){
+            for(k=i;k<=j;k++)printf("%d ",sor[k]);printf("\n");
             swap(sor+s,sor+i);
+            for(k=i;k<=j;k++)printf("%d ",sor[k]);printf("\n\n");
             downheap(sor,i,s-1,i);
         }
-        swap(sor+i+1,sor+i);
-
         return 1;
 }
 
@@ -58,6 +60,7 @@ int main(){
     scanf("%d",&N);
     int s[1000000];
     for(i=0;i<N;i++) scanf("%d",s+i);
+    for(int k=0;k<N;k++)printf("%d ",s[k]);printf("\n\n");
     clock_t begin=clock();
     heapsort(s,0,N-1);
     clock_t end=clock();
@@ -65,4 +68,3 @@ int main(){
     printf("time: %f\n",(double)(end-begin));
     return 0;
 }
-
