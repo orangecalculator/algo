5b7c66ab7c800d89c7765928129934922fb0fc59
orangecalculator <43122348+orangecalculator@users.noreply.github.com>
2018-12-25 00:18:51 +0900

    Escape Sequences Explanations

--- cprogramming/learningc/E001escapesequences.cpp
+++ cprogramming/learningc/E001escapesequences.cpp
@@ -8,19 +8,19 @@ int main(){
 	printf("To C, or not to C: that is the question.\n");
 	
 	// first example and exercise of KNR
-	printf("hello, world%c",0x0a);
+	printf("hello, world%c",0x0a);	// linefeed by ASCII code
 	
-    printf("a: \a f\n");
-	printf("b: erase\b f\n");
-	printf("e: \e f\n");
-	printf("f: \f f\n");
-	printf("n: \n f\n");
-	printf("r: \r \n");
-	printf("t: \t f\n");
-	printf("u: \u0167 f\n");
-	printf("v: ff\vff f\n");
-	printf("x: \x41 f\n");
-	printf("nnn: \101");
+    	printf("a: \a f\n");		// alert
+	printf("b: erase\b f\n");	// backspace
+	printf("e: \e f\n");		// escape characters (non-standard)
+	printf("f: \f f\n");		// formfeed page break
+	printf("n: \n f\n");		// linefeed line break
+	printf("r: \r \n");		// cursor return
+	printf("t: \t f\n");		// tab
+	printf("u: \u0167 f\n");	// unicode
+	printf("v: ff\vff f\n");	// vertical tab
+	printf("x: \x41 f\n");		// hexadecimal character code
+	printf("nnn: \101");		// octal character code
 	
 	return 0;
 }
