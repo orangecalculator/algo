640a29af4beacb1d4f1c71408081643da587f792
orangecalculator <smartman1996@gmail.com>
2019-05-23 22:34:42 +0900

    lab08 mosfet CD Amp

--- school/electronicdevices/lab08_CDAmp.R
+++ school/electronicdevices/lab08_CDAmp.R
@@ -68,3 +68,79 @@ ggplot(mapping=aes(Vin,Vout),data=CD_Amp)+
   geom_path(color="blue")+
   stat_function(fun = function(value) { Get_CD_Out(value,mu,Vth,VDD,R_real) },color="green")+
   labs(x="Gate Input Voltage[V]",y="Source Output Voltage[V]",title=element_text("Common Source Amplifier In-Out Characteristic"))
+
+Get_CD_Out_Body <- function(Vin,mu,Vth,gam,phi,VDD,RL) {
+  Vout <- rep(0,length(Vin))
+  for( k in seq(1,length(Vin)) ){
+    Vout[k] <- ifelse(Vin[[k]]<Vth,0,
+                      uniroot(function(V_gss){
+                        sq1 <- phi+V_gss
+                        sq2 <- V_gss*2/(mu*RL)
+                        if( sq1<0 || sq2 <0 ) return(1e100)
+                        else return(Vin[[k]]-V_gss-Vth-gam*sqrt(sq1)-sqrt(sq2))
+                      }, c(0,2))[[1]]
+    )
+  }
+  Vout
+}
+
+Get_CD_Out_Body <- function(Vin,mu,Vth,gam,phi,VDD,RL) {
+  Vout <- rep(0,length(Vin))
+  for( k in seq(1,length(Vin)) ){
+    Vout[k] <- ifelse(Vin[[k]]<Vth,0,
+                        tryCatch(uniroot(function(V_gss){
+                          sq2 <- V_gss*2/(mu*RL)
+                          if( phi<0 || sq2 <0 ) return(1e100)
+                          else return(Vin[[k]]-V_gss-Vth-gam*(sqrt(phi+V_gss)-sqrt(phi))-sqrt(sq2))
+                        }, c(0,VDD))[[1]],error=function(e) {VDD},warning=function(e) {VDD} )
+                        )
+  }
+  Vout
+}
+
+CD_Amp_fit <- CD_Amp
+fitting_obj <- optim(c(0.4,1),function(param) {sum(1e3*Get_CD_Out_Body(CD_Amp_fit$Vin,mu_prev,Vth_prev,param[[1]],param[[2]],VDD,R_real)-CD_Amp_fit$Vout)^2})
+
+ggplot(mapping=aes(Vin,Vout),data=CD_Amp)+
+  geom_point(color="blue")+
+  geom_path(color="blue")+
+  stat_function(fun = function(value) { Get_CD_Out_Body(value,mu,Vth,fitting_obj$par[[1]],fitting_obj$par[[2]],VDD,R_real) * 1e3 },color="green")+
+  labs(x="Gate Input Voltage[V]",y="Source Output Voltage[V]",title=element_text("Common Source Amplifier In-Out Characteristic"))
+
+fitting_obj <- optim(c(0.6,0.8),function(param) {
+    #if(param[[2]]<0) return(1e100)
+    sum((param[[1]]*sqrt(param[[2]]+CD_Amp$Vout/1e3)-(CD_Amp$Vin-CD_Amp$Vout/1e3-Vth_prev-sqrt(CD_Amp$Vout/1e3*2/(mu_prev*R_real))))^2)
+  })
+
+fitting_obj <- optim(c(0.8),function(param) {
+  #if(param[[2]]<0) return(1e100)
+  sum((param[[1]]*sqrt(CD_Amp$Vout/1e3)-(CD_Amp$Vin-CD_Amp$Vout/1e3-Vth_prev-sqrt(CD_Amp$Vout/1e3*2/(mu_prev*R_real))))^2)
+})
+
+fitting_obj <- optimize(function(param) {
+  #if(param[[2]]<0) return(1e100)
+  sum((param[[1]]*sqrt(CD_Amp$Vout/1e3)-(CD_Amp$Vin-CD_Amp$Vout/1e3-Vth_prev-sqrt(CD_Amp$Vout/1e3*2/(mu_prev*R_real))))^2)
+},c(0,1))
+
+gam <- fitting_obj$minimum
+
+Get_CD_Out_gam <- function(Vin,mu,Vth,gam,VDD,RL) {
+  VoltFactor <- gam + 1 / sqrt(mu*RL/2)
+  Voltsq <- VoltFactor^2-4*VoltFactor*(Vin-Vth)
+  Voltsq <- ifelse(Voltsq>0,Voltsq,0)
+  Vout <- (-VoltFactor + sqrt(Voltsq)) / 2 + Vin - Vth
+  ifelse(Vin<Vth,0,Vout * 1e3) # Out in [mV]
+}
+
+ggplot(mapping=aes(Vin,Vout),data=CD_Amp)+
+  geom_point(color="blue")+
+  geom_path(color="blue")+
+  stat_function(fun = function(value) { Get_CD_Out_gam(value,mu_prev,Vth_prev,gam,VDD,R_real)},color="green")+
+  labs(x="Gate Input Voltage[V]",y="Source Output Voltage[V]",title=element_text("Common Source Amplifier In-Out Characteristic"))
+
+fitting_obj <- optimize(function(param) {
+  #if(param[[2]]<0) return(1e100)
+  sum(Get_CD_Out_gam(CD_Amp$Vin,mu_prev,Vth_prev,param,VDD,R_real)-CD_Amp$Vout)^2
+},c(0,1))
+
+gam <- fitting_obj$minimum
